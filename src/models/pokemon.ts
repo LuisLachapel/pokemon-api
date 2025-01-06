2d85678b7d8b629c89c7ecac91087fba65abf93e
LuisLachapel <luislachapelcentropeguero@gmail.com>
2025-01-05 22:28:16 -0400

    Creacion del modelo e instalacion de react-router-dom

---
+++ src/models/pokemon.ts
@@ -0,0 +1,15 @@
+export type Pokemon = {
+  national_number: string;
+  name: string;
+  type: Array<any>;
+  total: number;
+  attack: number;
+  defense: number;
+  sp_atk: number;
+  sp_def: number;
+  speed: number;
+  hp: number;
+  imggif: string;
+  imglarge: string;
+  imgnormal: string;
+};
