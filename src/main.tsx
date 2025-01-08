b5b8653ea64739cc1aa03ef0838de3d954756a58
LuisLachapel <luislachapelcentropeguero@gmail.com>
2025-01-07 23:40:26 -0400

    Implementacion de chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from './components/ui/provider.tsx'
 import App from './App.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
+  <Provider>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </Provider>,
 )
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,9 +1,12 @@
-
+//import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 
 const List = () => {
   return (
   <>
+    <Button bg='tomato'>Click me</Button>
+
     <h1>Hola Peter</h1>
   </>);
 };
